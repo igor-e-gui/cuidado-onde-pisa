e092455953acf1bcdd4286282d59ca34ada4ada5
igor1023 <igor.s10@aluno.ifsc.edu.br>
2022-09-27 19:56:25 +0000

    Inclusão do tempo limite no código

--- ChatBot.py
+++ ChatBot.py
@@ -1,22 +1,13 @@
-# Dicionários do jogo
 from definições import estados, partidas
-#
-# Discord
 import discord
 from discord.ext import commands
-#
-# Aleatoriedade
 from random import choice
-#
-# Expressões regulares
 from re import fullmatch
-#
-# Variáveis de ambiente
+from datetime import datetime, timedelta
 from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
-# Definir o perfil do bot: ler mensagens.
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(intents=intents, command_prefix='')
@@ -29,63 +20,45 @@ async def on_ready():
 
 @bot.event
 async def on_message(msg):
-    #
-    # Armazenar o autor da mensagem.
+
     autor = msg.author.id
 
-    # Verificar se a mensagem não tem o próprio bot como autor.
     if autor == bot.application_id:
         return
-    #
-    # Verificar se o jogador ainda não começou a partida,
-    # o que significa que precisa colocá-lo no estado zero (0).
+
     if autor not in partidas:
-        partidas[autor] = 0
-    #
-    # Em ordem de operação:
-    # 0) Obter o estado atual do jogador:
-    #    partidas[autor]
-    # 1) Obter a definição completa desse estado:
-    #    estados[partidas[autor]]
-    estado_do_jogador = estados[partidas[autor]]
-    #
-    # 3) Filtrar desse estado apenas a lista de próximos estados:
-    #    estados_do_jogador['proximos_estados']
-    # 4) Obter a lista completa dos próximos estados:
-    #    estado_de_jogador['proximos_estados'].items()
-    # 5) Separar chave e valor da lista completa:
+        partidas[autor] = {'estado': 0, 'ultima_mensagem': msg.created_at}
+
+    estado_do_jogador = estados[partidas[autor]['estado']]
+    ultima_mensagem = partidas[autor]['ultima_mensagem']
+
     for key, value in estado_do_jogador['proximos_estados'].items():
-        #
-        # Comparar a frase do jogador com a chave usando expressões regulares:
+
         if fullmatch(key, msg.content):
-            #
-            # Atualizar o estado do jogador,
-            # e para isso é usado o conteúdo da mensagem como valor do dicionário:
-            partidas[autor] = value
-            #
-            # A definição completa do estado do jogador,
-            # por consequência, também é atualizada
-            estado_do_jogador = estados[partidas[autor]]
-            #
-            # Enviar para o jogador a mensagem do estado (já atualizado)
-            #
-            # Em ordem de operação:
-            # 0) Filtrar do estado do jogador apenas a lista de frases:
-            #    estado_do_jogador['frases']
-            # 1) Sortear uma frase dessa lista:
-            #   choice(estado_do_jogador['frases'])
+
+
+            if estado_do_jogador['tempo_limite'] > 0:
+
+                delta = msg.created_at - ultima_mensagem
+                if delta.seconds > estado_do_jogador['tempo_limite']:
+
+                    partidas[autor] = {
+                        'estado': 1,
+                        'ultima_mensagem': msg.created_at
+                    }
+                    await msg.channel.send('Tempo limite estourado (' + str(estado_do_jogador['tempo_limite']) + 's). Reiniciando o jogo...')
+                    return
+
+            partidas[autor]['estado'] = value
+            partidas[autor]['ultima_mensagem'] = msg.created_at
+            estado_do_jogador = estados[partidas[autor]['estado']]
+
             await msg.channel.send(choice(estado_do_jogador['frases']))
-            #
-            # Retonar a função, já que a resposta ao jogador já foi dada.
             return
-    #
-    # Caso contrário, avisar que a mensagem não avança no jogo.
-    # Se o jogador ainda estiver no estado, ajudar com uma dica:
-    if partidas[autor] == 0:
+
+    if partidas[autor]['estado'] == 0:
         await msg.channel.send(choice(estado_do_jogador['frases']))
     else:
-        #
-        #  Nos estados seguintes, a resposta padrão de HAL:
         await msg.channel.send('I\'m sorry Dave, I\'m afraid I can\'t do that.')
 
 
